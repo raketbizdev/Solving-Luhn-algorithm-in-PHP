f37428230c25c237f056bf1d347990f03dc256f8
radongrafix <radon1284@yahoo.com>
2015-12-09 11:34:51 +0800

    Update luhn_algorithm.php

--- luhn_algorithm.php
+++ luhn_algorithm.php
@@ -1,11 +1,13 @@
 <?php
 $ccd = '5105 1051 0510 5100';
-$strip_str =  str_replace(' ', '', $ccd);
-
+// To remove Space for Credit card
+$strip_str =  str_replace(' ', '', $ccd); 
+//Output or the string without space
 echo str_replace(' ', '', $ccd). '<br>';
+// Convert String into array
+$arr1 = str_split($strip_str); 
 
-$arr1 = str_split($strip_str);
-
+//Output individual string value
 foreach ($arr1 as $value) {
     echo "$value <br>";
 }
